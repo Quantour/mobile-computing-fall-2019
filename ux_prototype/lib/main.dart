771eaf370df71406f872fb1b6c00acd035fba54c
Emil Johansson <emil@Emils-MacBook-Pro.local>
2019-10-31 15:15:01 +0900

    Uploading non-working firebase application to new branch

--- ux_prototype/lib/main.dart
+++ ux_prototype/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ux_prototype/views/discover/discover.dart';
 import 'style.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 void main() => runApp(MyApp());
 
