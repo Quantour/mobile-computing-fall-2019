1cd3c723626195dd50e73a6063f841e84cba5653
andrelp <andre.leon.poncelet@gmail.com>
2019-12-05 19:04:53 +0900

    also deactivated rate and start button of a route, if user is not logged in

--- Wanderlust/lib/views/discover_detail/discover_detail.dart
+++ Wanderlust/lib/views/discover_detail/discover_detail.dart
@@ -78,9 +78,9 @@ class _DiscoverDetailState extends State<DiscoverDetail> {
                           CustomButton(
                             padding: const EdgeInsets.all(8),
                             margin: const EdgeInsets.all(8),
-                            color: CurrentHike.isActive?Colors.grey:null,
+                            color: (CurrentHike.isActive||!User.isLoggedIn)?Colors.grey:Theme.of(context).accentColor,
                             text: "Start",
-                            child: Icon(Icons.play_arrow, color: CurrentHike.isActive?Colors.grey:Theme.of(context).accentColor, size: 18,),
+                            child: Icon(Icons.play_arrow, color:(CurrentHike.isActive||!User.isLoggedIn)?Colors.grey:Theme.of(context).accentColor, size: 18,),
                             onPressed: () {
                               if (CurrentHike.isActive) {
                                 showDialog(
@@ -97,6 +97,21 @@ class _DiscoverDetailState extends State<DiscoverDetail> {
                                     ],
                                   )
                                 );
+                              } else if (!User.isLoggedIn) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      actions: <Widget>[
+                                        FlatButton(
+                                          child: Text("Ok"),
+                                          onPressed: ()=>Navigator.pop(context),
+                                        )
+                                      ],
+                                      content: Text("You must be logged in, in order to start a route!"),
+                                    );
+                                  }
+                                );
                               } else {
                                 Navigator.pop(context);
                                 CurrentHike.setActiveWithRoute(snapshot.data);
@@ -107,9 +122,27 @@ class _DiscoverDetailState extends State<DiscoverDetail> {
                           CustomButton(
                             padding: const EdgeInsets.all(8),
                             margin: const EdgeInsets.all(8),
+                            color: User.isLoggedIn? Theme.of(context).accentColor: Colors.grey,
                             text: "Rate",
-                            child: Icon(Icons.rate_review, color: Theme.of(context).accentColor, size: 18,),
+                            child: Icon(Icons.rate_review, color: User.isLoggedIn? Theme.of(context).accentColor: Colors.grey, size: 18,),
                             onPressed: () async {
+                              if (!User.isLoggedIn) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      actions: <Widget>[
+                                        FlatButton(
+                                          child: Text("Ok"),
+                                          onPressed: ()=>Navigator.pop(context),
+                                        )
+                                      ],
+                                      content: Text("You must be logged in, in order to rate a route!"),
+                                    );
+                                  }
+                                );
+                                return;
+                              }
                               await showRatingDialog(snapshot.data.routeID, context);
                               //update sum of rating when finished
                               setState(() {});
@@ -137,6 +170,7 @@ class _DiscoverDetailState extends State<DiscoverDetail> {
                                     );
                                   }
                                 );
+                                return;
                               }
                               widget.route.then((route){
                                 Navigator.push(
